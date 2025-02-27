80e0bcd6f516a8ef04c1c3023d91196803e05b8a
jaimyma <jaimy@direct-effect.nl>
2025-02-27 13:25:46 +0100

    More pages
    Made more pages

--- src/components/CommandInput.vue
+++ src/components/CommandInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <input type="text" class="w-full px-4 py-3 text-base/7 text-gray-900 placeholder-gray-500 bg-gray-100 border border-gray-300 rounded-lg focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none" placeholder="Type a command..." />
+  <input type="text" class="w-full px-4 py-3 text-base/7 text-gray-900 placeholder-gray-500 bg-gray-100 border border-gray-300 rounded-lg focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none" placeholder="Type a command..."/>
 </template>
 
 <script>
